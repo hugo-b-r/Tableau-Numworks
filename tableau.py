f4f40586650b07a10cf0c73eafa8e7efee256412
lemoustachu <lemoustrashu@outlook.com>
2021-03-19 18:00:06 +0100

    j'ai ajouté des commentaires et je suis passé à un système de disctinction entre les coordonnées de la case et celles par rapport à l'écran

--- tableau.py
+++ tableau.py
@@ -4,7 +4,7 @@ from kandinsky import *
 from ion import *
 from time import *
 
-#on dessine le cadre du tableau
+#on dessine le cadre du tableau et les numéros de coordonnées
 def colonnes():
   #les colonnes
   x_colonnes = 0
@@ -21,7 +21,7 @@ def lignes():
   draw_line(0, 221, 340, 221, color(0,0,0))
 
 def texte():
-  #le texte
+  #les numéros
   x_texte = 0
   y_texte = 1
   for i in range(9):
@@ -35,50 +35,72 @@ def texte():
     x_texte = x_texte + 64
     y_texte = y_texte + 1
 
-def selectionner():
-  fill_rect(x_selecteur,y_selecteur,63,21,color(220,220,220))
-      
 def quadrillage():
+  #appelle toute les fonctions qui dessinent le quadrillage et les coordonnées
   fill_rect(0,0,320,222,color(255,255,255))
   texte()
   colonnes()
   lignes()
 
-def effacer_la_selection():
-  fill_rect(x_selecteur,y_selecteur,63,21,color(255,255,255))  
 
+
+
+#on definit les fonctions pour passer des coordonnées de cases vers des coordonnées en pixels
+def x_case_vers_x_sur_l_ecran(x):
+  return x*64+1
+
+def y_case_vers_x_sur_l_ecran(y):
+  return y*22+1
+
+
+
+
+def selectionner():
+  #colorise le rectangle qui est sélectionné
+  fill_rect(x_case_vers_x_sur_l_ecran(x_selecteur),y_case_vers_x_sur_l_ecran(y_selecteur),63,21,color(220,220,220))
+
+def effacer_la_selection():
+  #efface l'ancienne sélection
+  fill_rect(x_case_vers_x_sur_l_ecran(x_selecteur),y_case_vers_x_sur_l_ecran(y_selecteur),63,21,color(255,255,255))  
+
+      
+
+#on initialise le tableau
 quadrillage()
 
-x_selecteur = 65
-y_selecteur = 23
+#on donne la première position du tableau
+x_selecteur = 1
+y_selecteur = 1
+#on affiche la selection
 sel()
 
 while True:
+  #on teste les touches de navigation
   if keydown(KEY_DOWN):#↓
-    if y_selecteur != 199:
+    if y_selecteur != 9:
       effacer_la_selection()
-      y_selecteur = y_selecteur + 22
+      y_selecteur = y_selecteur + 1
       sel()
       sleep(0.1)
       
   elif keydown(KEY_UP):#↑
-    if y_selecteur != 23:
+    if y_selecteur != 1:
       effacer_la_selection()
-      y_selecteur = y_selecteur - 22
+      y_selecteur = y_selecteur - 1
       sel()
       sleep(0.1)
   
   elif keydown(KEY_LEFT):#←
-    if x_selecteur != 65:
+    if x_selecteur != 1:
       effacer_la_selection()
-      x_selecteur = x_selecteur - 64
+      x_selecteur = x_selecteur - 1
       sel()
       sleep(0.15)
   
   elif keydown(KEY_RIGHT):#→
-    if x_selecteur != 257:
+    if x_selecteur != 4:
       effacer_la_selection()
-      x_selecteur = x_selecteur + 64
+      x_selecteur = x_selecteur + 1
       sel()
       sleep(0.15)
    elif keydown(KEY_ONE):
